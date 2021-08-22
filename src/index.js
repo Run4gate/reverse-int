66b705b5e6084f3947f2fce6960fd59f5ff9e9f1
Run4gate <westernisator@gmail.com>
2021-08-22 19:50:49 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let a = Math.abs(n)
+  let str = a.toString()
+  let lng = str.length
+  let i = 0
+  let result = ''
+  while (i < lng) {
+      result = str[i] + result
+      i++
+  }
+  return +result
 }
